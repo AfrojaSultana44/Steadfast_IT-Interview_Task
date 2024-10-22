c3d88a6c7f0ca1d57fc894fc9078544468c82bef
AfrojaSultana44 <afrojasultanamoni311@gmail.com>
2024-10-23 00:52:12 +0600

    Task-1 completed

--- Task-1/js/index.js
+++ Task-1/js/index.js
@@ -2,40 +2,18 @@ const openIcon = document.getElementById("mobile-menu-open");
 
 const closeIcon = document.getElementById("mobile-menu-close");
 
-const desktopMenu = document.getElementById("desktop-menu");
-
 const mobileMenu = document.getElementById("mobile-menu");
 
 openIcon.addEventListener("click", () => {
-
-  console.log("mobileMenu:", mobileMenu);
-
-
-  openIcon.classList.remove("display-block");
-  openIcon.classList.add("display-hidden");
-
-  closeIcon.classList.add("display-block");
-
-  mobileMenu.classList.remove("display-hidden");
-
-  mobileMenu.classList.add("display-block");
-
-  desktopMenu.classList.remove("display-block");
-  desktopMenu.classList.add("display-hidden");
+  mobileMenu.style.display = "inline-block";
 });
 
 closeIcon.addEventListener("click", () => {
-  console.log("mobileMenu:", mobileMenu);
+  mobileMenu.style.display = "none";
+});
 
-  closeIcon.classList.remove("display-block");
-  closeIcon.classList.add("display-hidden");
-
-  openIcon.classList.add("display-block");
-
-  desktopMenu.classList.remove("display-hidden");
-
-  desktopMenu.classList.add("display-block");
-
-  mobileMenu.classList.remove("display-block");
-  mobileMenu.classList.add("display-hidden");
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 767) {
+    mobileMenu.style.display = "none";
+  }
 });
